fcc92fac38770faf67595dd3e513c9e7af78459b
Rahul Muthusamy <Rahul.Muthusamy@st-francis.org>
2021-09-21 18:35:38 +0530

    creating auto-updating

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const {  BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
 
+const app = require('electron').app;
+
+Object.defineProperty(app, 'isPackaged', {
+  get() {
+    return true;
+  }
+});
 let mainWindow;
 
 function createWindow () {
@@ -14,19 +21,23 @@ function createWindow () {
   });
   mainWindow.webContents.openDevTools();
   mainWindow.loadFile('index.html');
+
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
+
   mainWindow.once('ready-to-show', () => {
     console.log('ready-to-show');
     autoUpdater.checkForUpdatesAndNotify();
   });
+
 }
 
 app.on('ready', () => {
   createWindow();
-  autoUpdater.checkForUpdatesAndNotify();
 });
+ 
+  
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
@@ -44,24 +55,31 @@ ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
 });
 
-autoUpdater.on("checking-for-update", () => {
-  console.log('checking-for-update'); 
-});
-/*No updates available*/
-autoUpdater.on("update-not-available", info => {
-  console.log('update-not-available');
-});
-
 autoUpdater.on('update-available', () => {
-  console.log('update-available');
-  mainWindow.webContents.send('update_available');
+  console.log('update-available'); 
 });
-
-
 autoUpdater.on('update-downloaded', () => {
-  console.log('update-downloaded');
-  mainWindow.webContents.send('update_downloaded');
+  console.log('update-downloaded'); 
 });
+
+// autoUpdater.on("checking-for-update", () => {
+//   console.log('checking-for-update'); 
+// });
+ 
+// autoUpdater.on("update-not-available", () => {
+//   console.log('update-not-available');
+// });
+
+// autoUpdater.on('update-available', () => {
+//   console.log('update-available');
+//   mainWindow.webContents.send('update_available');
+// });
+
+
+// autoUpdater.on('update-downloaded', () => {
+//   console.log('update-downloaded');
+//   mainWindow.webContents.send('update_downloaded');
+// });
 
 ipcMain.on('restart_app', () => {
   autoUpdater.quitAndInstall();
